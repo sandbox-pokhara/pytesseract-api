aa88e41d3fe4dff2c22527f4f53a0003a29c9b23
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-04-18 15:29:04 +0545

    chore: bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytesseract-api",
-    version="1.0.1",
+    version="1.0.2",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description=(
